8785c0ac7a3cff25c997ec74b46f24008b2a69bd
garymiller <77537560+repro-tetris@users.noreply.github.com>
2021-06-25 09:41:37 +0300

    change onload

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import menuData from './menu.json';
 import menuItemsTpl from './templates/menu-items.hbs';
 import './sass/styles.scss';
 
-window.onload = () => {
+document.addEventListener('DOMContentLoaded', () => {
   setThemeSwitcher('#theme-switch-toggle');
   document.querySelector('.js-menu').innerHTML = menuItemsTpl(menuData);
-};
+});
